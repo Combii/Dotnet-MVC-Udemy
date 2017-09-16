54ead570c2f3525994f529206a2c94f34d22ec9d
Combii <davidstovle@gmail.com>
2017-09-16 17:29:50 +0200

    Unused stuff

--- Startup.cs
+++ Startup.cs
@@ -41,8 +41,8 @@ namespace Dotnet_MVC_Vidly
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    "default",
+                    "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }

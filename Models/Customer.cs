2d9f1e0c6eb33b30ff16761e50e70389486a88cb
Combii <davidstovle@gmail.com>
2017-09-16 16:50:15 +0200

    Working commit

---
+++ Models/Customer.cs
@@ -0,0 +1,8 @@
+﻿namespace Dotnet_MVC_Vidly.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Dotnet_MVC_Vidly.Models;
+
+namespace Dotnet_MVC_Vidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}

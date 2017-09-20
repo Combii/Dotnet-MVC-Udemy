e5b99ba70445a0fe423d6165c07aec35d986002a
Combii <davidstovle@gmail.com>
2017-09-20 16:57:30 +0200

    Working db

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Dotnet_MVC_Vidly.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,31 +6,35 @@ namespace Dotnet_MVC_Vidly.Controllers
 {
     public class CustomersController : Controller
     {
-                
+
+        private CustomersList _context;
+
+        public CustomersController()
+        {
+            _context = new CustomersList();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers;
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return NotFound();
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "John Smith"},
-                new Customer {Id = 2, Name = "Mary Williams"}
-            };
         }
     }
 }

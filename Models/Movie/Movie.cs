3e1131619343b17a44c708964c4b9fcd0dd9ce2e
Combii <davidstovle@gmail.com>
2017-10-08 12:27:08 +0200

    Working edit Movie

--- Models/Movie/Movie.cs
+++ Models/Movie/Movie.cs
@@ -6,10 +6,10 @@ namespace Dotnet_MVC_Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        
         [Required]
         public string Name { get; set; }
         
-        [Required]
         public Genre Genre{ get; set; }
                 
         public DateTime ReleaseDate { get; set; }

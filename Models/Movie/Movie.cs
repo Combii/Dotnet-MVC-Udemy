a342448c639d800bf0715f369bbfe3dba13cf340
Combii <davidstovle@gmail.com>
2017-10-20 18:23:57 +0200

    Working validation for Movies

--- Models/Movie/Movie.cs
+++ Models/Movie/Movie.cs
@@ -19,7 +19,7 @@ namespace Dotnet_MVC_Vidly.Models
         public DateTime DateAdded { get; set; }
         
         [Display (Name = "Number in Stock")]
-        [Required]
+        [StockMustBe1to20]
         public int? NumberOfStock { get; set; }
     }
 }
---
+++ Models/Movie/StockMustBe1to20.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dotnet_MVC_Vidly.Models
+{
+    public class StockMustBe1to20 : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var movie = (Movie) validationContext.ObjectInstance;
+
+            if (movie.NumberOfStock == null)
+                return new ValidationResult("The Number in Stock field is required.");
+
+            if (movie.NumberOfStock > 0 && movie.NumberOfStock <= 20 && movie.NumberOfStock != null)
+                return ValidationResult.Success;
+            
+            return new ValidationResult("The field Number in Stock must be between 1 and 20");
+        }
+    }
+}

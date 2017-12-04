56fd2aab5e8ed8c0039a7d670954ffb95717b3a3
Combii <davidstovle@gmail.com>
2017-12-04 17:45:32 +0100

    Added repos

---
+++ Models/Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dotnet_MVC_Vidly.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_MVC_David.Data.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomerRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.Include(c => c.MembershipType).ToList();
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            return _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == customerId);
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+            _context.SaveChanges();
+        }
+
+        public Customer DeleteCustomer(int customerId)
+        {
+            var customer = GetCustomerById(customerId);
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+
+            return customer;
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            InsertCustomer(customer);
+        }
+
+        
+    }
+}
---
+++ Models/Data/Repositories/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dotnet_MVC_Vidly.Models;
+
+namespace Dotnet_MVC_David.Data.Repositories
+{
+    public interface ICustomerRepository
+    {
+        IEnumerable<Customer> GetCustomers();
+        Customer GetCustomerById(int customerId);
+        void InsertCustomer(Customer customer);
+        Customer DeleteCustomer(int customerId);
+        void UpdateCustomer(Customer customer);
+       
+    }
+}
